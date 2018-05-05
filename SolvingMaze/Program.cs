1df8846811f80cccd064a23e5376696edb7c795b
Yu-Jin <yujinost@gmail.com>
2018-05-05 16:42:13 +0100

    initial setup of parsing character by character of small.txt

--- SolvingMaze/Program.cs
+++ SolvingMaze/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace SolvingMaze
 {
     class Program
     {
+        static void ReadMazeFile()
+        {
+            string path = @"c:\small.txt";
+            string[] fileLine = File.ReadAllText(path).Trim().Split('\n');
+
+            string mapWidthHeight = fileLine[0];
+            string startXY = fileLine[1];
+            string endXY = fileLine[2];
+
+            for (int i = 3; i < fileLine.Length; i++)
+            {
+                //Console.WriteLine(fileLine[i]); //shows one line
+                for(int j = 0; j <  fileLine[i].Length; j++)
+                {
+                    Console.WriteLine(fileLine[i][j]); //shows each character of each line
+                }
+            }
+
+        }
+
         static void Main(string[] args)
         {
+            ReadMazeFile();
         }
     }
 }
